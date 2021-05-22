cd6aaac7605ead99160cf716018b7677b4127f42
Brian Retamar <leo1632@hotmail.com>
2021-05-22 16:26:35 -0300

    changed db design, added category as fk

--- sql/db.sql
+++ sql/db.sql
@@ -1,12 +1,50 @@
 CREATE DATABASE blog_db;
 
+USE blog_db;
+
+CREATE TABLE categories (
+    id TINYINT(255) PRIMARY KEY AUTO_INCREMENT,
+    description VARCHAR(100)
+);
 
 CREATE TABLE posts ( 
-                        id INT NOT NULL AUTO_INCREMENT primary key,
-                        title VARCHAR(100),
-                        content VARCHAR(200),
-                        image VARCHAR(200),
-                        category VARCHAR(100),
-                        date Date 
-                    );
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL CHECK (title <> ''),
+    content TEXT NOT NULL CHECK (content <> ''),
+    image VARCHAR(200),
+    category TINYINT(255),
+    date Date NOT NULL,
+    CONSTRAINT fk_posts_category 
+    FOREIGN KEY (category) REFERENCES categories(id)
+);
 
+INSERT INTO categories (description) 
+    VALUES
+    ('programming'),('videogames'), ('social networks'), ('series'), ('random'),  ('other')
+;
+
+INSERT INTO posts(title, content, image, category, date) 
+    VALUES('Hello Alkemy', 'Hola pa, que haces?', 
+        "https://cdn.generadormemes.com/media/created/n1kchrjzihqkjdefptelyersza565wijxfacfeymomvddbwfkli2qiqslma35ty.jpg",
+        5, NOW()
+    );
+
+INSERT INTO posts(title, content, image, category ,date) 
+VALUES('C# is cool (pt1)', 'Googleando me encontré esto WTF xD', 
+        "https://m.media-amazon.com/images/I/51GsWzw+boL.jpg", 1, NOW()
+    );
+
+INSERT INTO posts(title, content, image, category, date) 
+VALUES('C# is cool (pt2)', 'Googleando me encontré esto WTF xD', 
+        "https://m.media-amazon.com/images/I/51KLmiCkOuL.jpg", 1, NOW()
+    );
+
+
+SELECT 
+    posts.id, posts.title, posts.content, posts.image, posts.date, categories.description AS category
+     FROM posts INNER JOIN categories ON posts.category = categories.id;
+
+SELECT 
+    posts.id, posts.title, posts.content, posts.image, posts.date, categories.description AS category
+     FROM posts INNER JOIN categories ON posts.category = categories.id
+WHERE posts.id = ?;
